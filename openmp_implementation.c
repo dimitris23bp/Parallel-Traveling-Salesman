b68a9974b076ea733811040b35dd02f2e1318c6d
grode23 <dimitris23bp@gmail.com>
2019-10-06 23:40:52 +0300

    OpenMP works properly

--- openmp_implementation.c
+++ openmp_implementation.c
@@ -1,26 +1,46 @@
 #include <stdio.h>
 #include <limits.h>
 #include <string.h>
-#include <pthread.h>
 #include <omp.h>
+#include <time.h>
 
 #include "matrix_generator.h"
 
-#define N 4
-#define NUM_OF_THREADS 2
+#define N 20
+#define NUM_OF_THREADS 4
+
+//Example path - May not be used 
+int adj[N][N] = {{0,13,51,18,15,83,27,44,69,34,15,44,35, 52, 27, 70, 64, 61, 9, 88}, 
+{13, 0, 33, 3, 28, 11, 56, 3, 64, 18, 61, 6, 75, 76, 43, 49, 44, 73, 73, 92},
+{51, 33, 0, 88, 15, 53, 12, 44, 62, 95, 23, 88, 79, 14, 71, 15, 68, 69, 33, 16},
+{18, 39 ,88 ,0, 14, 87, 96, 94 ,34 ,48 ,78 ,35 ,30 ,63 ,94 ,77 ,58 ,37 ,21 ,30},
+{15, 28, 15, 14, 0 ,57 ,37 ,41, 66, 31, 8 ,27 ,23 ,86 ,36 ,52 ,66 ,25 ,66 ,28},
+{83, 11, 53, 87, 57, 0 ,81 ,83, 94, 30, 12, 3 ,61 ,65 ,85 ,90 ,61 ,11 ,82 ,52},
+{27, 56, 12, 96, 37, 81, 0 ,40, 3 ,59 ,10 ,97 ,11 ,29 ,29 ,90 ,93 ,79 ,14 ,18},
+{44, 3 ,44 ,94 ,41 ,83 ,40 ,0 ,68 ,35 ,51 ,60 ,33 ,38 ,11 ,67 ,50 ,43 ,20 ,81},
+{69, 64, 62, 34, 66, 94, 3 ,68, 0 ,68 ,14 ,68 ,95 ,60 ,63 ,75 ,41 ,37 ,98 ,4},
+{34, 18, 95, 48, 31, 30, 59, 35, 68, 0, 53, 42, 78, 86, 10, 6 ,3, 84, 15, 38},
+{15, 61, 23, 78, 8 ,12 ,10 ,51, 14, 53, 0 ,62 ,99 ,25 ,85 ,95 ,65 ,89 ,7, 22},
+{44, 6 ,88 ,35 ,27 ,3, 97, 60 ,68 ,42 ,62 ,0, 7, 47, 76, 65, 2 ,1, 89, 20},
+{35, 75, 79, 30, 23, 61, 11, 33, 95, 78, 99, 7, 0 ,12 ,72 ,18 ,87 ,53 ,88 ,4},
+{52, 76, 14, 63, 86, 65, 29, 38, 60, 86, 25, 47, 12, 0, 17, 32, 40, 20, 61, 82},
+{27, 43, 71, 94, 36, 85, 29, 11, 63, 10, 85, 76, 72, 17, 0, 89, 73, 42, 50, 44},
+{70, 49, 15, 77, 52, 90, 90, 67, 75, 6, 95, 65, 18, 32, 89, 0 ,3, 16, 12, 36},
+{64, 44, 68, 58, 66, 61, 93 ,50, 41, 3, 65, 2 ,87 ,40 ,73 ,30, 15, 82, 54, 78},
+{61, 73, 69, 37, 25, 11, 79, 43, 37, 8, 48, 9, 53, 20, 42, 16, 15, 0 ,38 ,61},
+{9 ,73 ,33 ,21 ,66 ,82 ,14 ,20, 98, 15, 7 ,89 ,88 ,61 ,50 ,12 ,82 ,38 ,0, 70},
+{88, 92, 16, 30, 28, 52, 18, 81, 4, 38, 22, 20, 4, 82, 44 ,36, 54, 61, 70, 0}};
 
 unsigned int final_path[N+1]; 
-int visited[N]; 
 unsigned int final_res = UINT_MAX; 
 
 void copyToFinal(int* curr_path) 
 { 
 	for (int i = 0; i < N; i++) {
-		final_path[i] = curr_path[i];	
+		final_path[i] = curr_path[i];
 	} 
-	
+
 	final_path[N] = curr_path[0]; 
-	
 } 
   
 int firstMin(int adj[N][N], int i) 
@@ -55,16 +75,17 @@ int secondMin(int adj[N][N], int i)
 	return second; 
 } 
 
-void TSPRec(int adj[N][N], int curr_bound, int curr_weight, int level, int curr_path[N+1], int id){ 
+void recursion(int adj[N][N], int curr_bound, int curr_weight, int level, int curr_path[N+1], int visited[N]){ 
 
 	if (level == N){ 
-
 		if (adj[curr_path[level - 1]][curr_path[0]] != 0){ 
 			int curr_res = curr_weight + adj[curr_path[level-1]][curr_path[0]]; 
-	  
-			if (curr_res < final_res){ 
-				copyToFinal(curr_path); 
-				final_res = curr_res; 
+	  		#pragma omp critical
+	  		{
+				if (curr_res < final_res){
+					copyToFinal(curr_path); 
+					final_res = curr_res; 
+				}
 			} 
 		} 			
 
@@ -73,96 +94,110 @@ void TSPRec(int adj[N][N], int curr_bound, int curr_weight, int level, int curr_
 	} 
 
 
-		for (int i = omp_get_thread_num(); i < N; i++){ 
-			if (adj[curr_path[level-1]][i] != 0 && visited[i] == 0){ 
-				int temp = curr_bound; 
-				curr_weight += adj[curr_path[level - 1]][i]; 
-	  
+	for (int i = 0; i < N; i++){ 
+		if (adj[curr_path[level-1]][i] != 0 && visited[i] == 0){ 
+			int temp = curr_bound; 
+			curr_weight += adj[curr_path[level - 1]][i]; 
+  
+			if (level==1) {
+				curr_bound -= ((firstMin(adj, curr_path[level - 1]) + firstMin(adj, i))/2); 
+			} else{
+				curr_bound -= ((secondMin(adj, curr_path[level - 1]) + firstMin(adj, i))/2); 
+  			}
 
+			if (curr_bound + curr_weight < final_res){ 
+				curr_path[level] = i; 
+				visited[i] = 1; 
+  
+				recursion(adj, curr_bound, curr_weight, level + 1, curr_path, visited);
 
-				if (level==1) {
-					curr_bound -= ((firstMin(adj, curr_path[level - 1]) + firstMin(adj, i))/2); 
-				} else{
-					curr_bound -= ((secondMin(adj, curr_path[level - 1]) + firstMin(adj, i))/2); 
-	  			}
-	  			//printf("I'm going to %d with curr_bound %d, weight %d and level %d\n", i+1, curr_bound, curr_weight, level);
-
-
-				if (curr_bound + curr_weight < final_res){ 
-					curr_path[level] = i; 
-					#pragma omp critical
-					{
-						visited[i] = 1; 
-					}
-	  				
-					TSPRec(adj, curr_bound, curr_weight, level + 1, curr_path, id); 
-				} 
-	  
-				curr_weight -= adj[curr_path[level-1]][i]; 
-				curr_bound = temp; 
-			  	
-			  	#pragma omp critical
-				{
-	 				memset(visited, 0, sizeof(visited));
-					for (int j = 0; j <= level - 1; j++) {
-						visited[curr_path[j]] = 1; 
-					}
-				}
-
-			
+			} 
+  
+			curr_weight -= adj[curr_path[level-1]][i]; 
+			curr_bound = temp; 
+		  	
+ 			memset(visited, 0, sizeof(*visited)*N);
+			for (int j = 0; j <= level - 1; j++) {
+				visited[curr_path[j]] = 1; 
+			}	
 
 		}
 	}
 }
+
+
+void second_node(int adj[N][N], int curr_bound, int curr_weight, int level, int curr_path[N+1], int visited[N]){
+
+	for (int j = omp_get_thread_num()+1; j < N; j+=NUM_OF_THREADS){
+		
+		int temp = curr_bound; 
+		curr_weight += adj[curr_path[level - 1]][j]; 
+		curr_bound -= ((firstMin(adj, curr_path[level - 1]) + firstMin(adj, j))/2); 
+  			
+		curr_path[level] = j; 
+		visited[j] = 1; 
+
+		recursion(adj, curr_bound, curr_weight, 2, curr_path, visited); 
+
+		curr_weight = 0;
+		curr_bound = temp; 
+ 		memset(visited, 0, sizeof(*visited)*N);
+ 		visited[0] = 1;
+
+	}
+
+
+}
   
-void TSP(int adj[N][N]){
+void first_node(int adj[N][N]){
 
-	int curr_path[N+1]; 
 
- 	memset(curr_path, -1, sizeof(curr_path));
- 	memset(visited, 0, sizeof(visited));
-
-	int curr_bound = 0; 
+	int init_bound = 0; 
   
 	for (int i = 0; i < N; i++) {
-		curr_bound += (firstMin(adj, i) + secondMin(adj, i)); 
+		init_bound += (firstMin(adj, i) + secondMin(adj, i)); 
 	}
 
-	if(curr_bound == 1){
-		curr_bound = curr_bound / 2 + 1;
+	if(init_bound == 1){
+		init_bound = init_bound / 2 + 1;
 	} else {
-		curr_bound = curr_bound / 2; 
+		init_bound = init_bound / 2; 
 	}
-
-	visited[0] = 1; 
-	curr_path[0] = 0; 
   
 
 	omp_set_num_threads(NUM_OF_THREADS);
 	#pragma omp parallel
 	{
+		//Declare curr_path and set it to start from 0 node
+		int curr_path[N+1]; 
+	 	memset(curr_path, -1, sizeof(curr_path));
+		curr_path[0] = 0; 
+
+		//Declare visited nodes and set it to have visited 
+		int visited[N]; 
+ 		memset(visited, 0, sizeof(visited));
+		visited[0] = 1; 
+
+
+	 	int curr_bound = init_bound;
+
 		int id = omp_get_thread_num();
 
-		TSPRec(adj, curr_bound, 0, 1, curr_path, id); 
+		second_node(adj, curr_bound, 0, 1, curr_path, visited); 
 
 	}
 
 } 
 
 int main(int argc, char const *argv[]){
-	//Adjacency matrix for the given graph 
-	//int adj[N][N] = {{0, 10, 15, 20}, {10, 0, 35, 25}, {15, 35, 0, 30}, {20, 25, 30, 0} }; 
-
-	int adj[N][N];
 
 	generator(N, adj, 100);
 	display(N, adj);
 
 	//Starting time of solution
-	clock_t begin = clock();
+	double start = omp_get_wtime();
 
-
-	TSP(adj);
+	first_node(adj);
    	 
     printf("Minimum cost : %d\n", final_res); 
 	printf("Path Taken : "); 
@@ -172,11 +207,10 @@ int main(int argc, char const *argv[]){
 
 	printf("\n");
 
-	clock_t end = clock();
-	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	//Finishing time of solution
+	double finish = omp_get_wtime();
 
-	printf("Time spent: %f\n", time_spent);
-
+	printf("Time spent: %f\n", finish - start);
 
 	return 0;
 }
