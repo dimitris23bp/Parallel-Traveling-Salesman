c3f2b494f1208cc181e0963d8bd8468b85cbef42
grode <dimitris23bp@gmail.com>
2020-03-23 16:36:04 +0200

    Added the first attempt for 3D graphs

---
+++ graphs/main.py
@@ -0,0 +1,59 @@
+"""
+=============================================
+Generate polygons to fill under 3D line graph
+=============================================
+
+Demonstrate how to create polygons which fill the space under a line
+graph. In this example polygons are semi-transparent, creating a sort
+of 'jagged stained glass' effect.
+"""
+
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.collections import PolyCollection
+import matplotlib.pyplot as plt
+from matplotlib import colors as mcolors
+import numpy as np
+
+# Creation of the 3D model
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+
+def cc(arg):
+    return mcolors.to_rgba(arg, alpha=0.6)
+
+# The range of size in which the graphs will be displayed 
+xs = np.arange(0, 11)
+
+verts = []
+
+# The proccessor that the graph will be placed
+zs = [1, 2, 3, 4]
+
+# Repeat for every different graph
+for z in zs:
+    ys = [0, 3.1, .2, .6, .5, .3, 3.2, .7, .2, .9, .4]
+
+    # indexes 0 and -1 are equal to 0 because otherwise the graphs would float
+    ys[0], ys[-1] = 0, 0
+
+    # Add to list the values and the place in their graph (the size)
+    verts.append(list(zip(xs, ys)))
+
+
+poly = PolyCollection(verts, facecolors=[cc('r'), cc('g'), cc('b'), cc('y')])
+
+#adiafaneia
+poly.set_alpha(0.7)
+ax.add_collection3d(poly, zs=zs, zdir='y')
+
+#Label axes and give the scale of them
+ax.set_xlabel('Size')
+ax.set_xlim3d(0, 10)
+ax.set_ylabel('Proccessors')
+ax.set_ylim3d(1, 5)
+ax.set_zlabel('Time')
+ax.set_zlim3d(0, 4)
+
+# display the model
+plt.show()
