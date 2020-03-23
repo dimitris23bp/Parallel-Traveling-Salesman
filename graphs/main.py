e02fc9c95d7e2699206c4bb49de3902c757e5c9c
grode <dimitris23bp@gmail.com>
2020-03-23 17:11:37 +0200

    More accessible code

--- graphs/main.py
+++ graphs/main.py
@@ -1,13 +1,3 @@
-"""
-=============================================
-Generate polygons to fill under 3D line graph
-=============================================
-
-Demonstrate how to create polygons which fill the space under a line
-graph. In this example polygons are semi-transparent, creating a sort
-of 'jagged stained glass' effect.
-"""
-
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.collections import PolyCollection
 import matplotlib.pyplot as plt
@@ -19,8 +9,8 @@ fig = plt.figure()
 ax = fig.gca(projection='3d')
 
 
-def cc(arg):
-    return mcolors.to_rgba(arg, alpha=0.6)
+def cc(arg, alpha=0.6):
+	return mcolors.to_rgba(arg, alpha=alpha)
 
 # The range of size in which the graphs will be displayed 
 xs = np.arange(0, 11)
@@ -28,23 +18,28 @@ xs = np.arange(0, 11)
 verts = []
 
 # The proccessor that the graph will be placed
-zs = [1, 2, 3, 4]
+zs = [1, 2, 3, 4, 1]
 
+flag = True
 # Repeat for every different graph
 for z in zs:
-    ys = [0, 3.1, .2, .6, .5, .3, 3.2, .7, .2, .9, .4]
+	if flag == True:
+		ys = [0, 3.1, 1.2, 1.6, 1.5, 2.3, 1.2, .7, .2, .9, .4]
+		flag = False
+	else:
+		ys = [0, 3.1, .2, .6, .5, .3, 3.2, .7, .2, .9, .4]
 
-    # indexes 0 and -1 are equal to 0 because otherwise the graphs would float
-    ys[0], ys[-1] = 0, 0
+	# indexes 0 and -1 are equal to 0 because otherwise the graphs would float
+	ys[0], ys[-1] = 0, 0
 
-    # Add to list the values and the place in their graph (the size)
-    verts.append(list(zip(xs, ys)))
+	# Add to list the values and the place in their graph (the size)
+	verts.append(list(zip(xs, ys)))
 
 
-poly = PolyCollection(verts, facecolors=[cc('r'), cc('g'), cc('b'), cc('y')])
+poly = PolyCollection(verts, facecolors=[cc('white'), cc('g'), cc('b'), cc('purple'), cc('black', 1)])
 
 #adiafaneia
-poly.set_alpha(0.7)
+#poly.set_alpha(0.7)
 ax.add_collection3d(poly, zs=zs, zdir='y')
 
 #Label axes and give the scale of them
