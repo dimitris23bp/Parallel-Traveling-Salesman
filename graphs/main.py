7c8d09807e367332dfbd9bed3f975b13d0751d1b
grode <dimitris23bp@gmail.com>
2020-03-29 14:22:40 +0300

    First 'correct' example of graph

--- graphs/main.py
+++ graphs/main.py
@@ -4,51 +4,97 @@ import matplotlib.pyplot as plt
 from matplotlib import colors as mcolors
 import numpy as np
 
-# Creation of the 3D model
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-
-
 def cc(arg, alpha=0.6):
 	return mcolors.to_rgba(arg, alpha=alpha)
 
-# The range of size in which the graphs will be displayed 
-xs = np.arange(0, 11)
+def items():
 
-verts = []
+	# First column is threads
+	# Second column is size of problem
+	# Third column is execution time
 
-# The proccessor that the graph will be placed
-zs = [1, 2, 3, 4, 1]
+	with open('results.txt', 'r') as f:
+		content = f.readlines()
 
-flag = True
-# Repeat for every different graph
-for z in zs:
-	if flag == True:
-		ys = [0, 3.1, 1.2, 1.6, 1.5, 2.3, 1.2, .7, .2, .9, .4]
-		flag = False
-	else:
-		ys = [0, 3.1, .2, .6, .5, .3, 3.2, .7, .2, .9, .4]
+	# Every data I need, but as strings
+	content = [x.strip().split() for x in content]
+	
+	results = []
 
-	# indexes 0 and -1 are equal to 0 because otherwise the graphs would float
-	ys[0], ys[-1] = 0, 0
+	for item in content:
+		curr_result = []	
+		curr_result.append(int(item[0]))
+		curr_result.append(int(item[1]))
+		curr_result.append(float(item[2]))
+		results.append(curr_result)
 
-	# Add to list the values and the place in their graph (the size)
-	verts.append(list(zip(xs, ys)))
+	return results
 
 
-poly = PolyCollection(verts, facecolors=[cc('white'), cc('g'), cc('b'), cc('purple'), cc('black', 1)])
+def main():
+	# Creation of the 3D model
+	fig = plt.figure()
+	ax = fig.gca(projection='3d')
 
-#adiafaneia
-#poly.set_alpha(0.7)
-ax.add_collection3d(poly, zs=zs, zdir='y')
+	# The range of size in which the graphs will be displayed 
+	xs = np.arange(10, 16)
 
-#Label axes and give the scale of them
-ax.set_xlabel('Size')
-ax.set_xlim3d(0, 10)
-ax.set_ylabel('Proccessors')
-ax.set_ylim3d(1, 5)
-ax.set_zlabel('Time')
-ax.set_zlim3d(0, 4)
+	print(xs)
+	verts = []
 
-# display the model
-plt.show()
+	# The proccessor that the graph will be placed
+	zs = [1, 2, 3, 4]
+	content = items()
+
+	# Repeat for every different graph
+	for z in zs:
+
+		ys = []
+		ys.append(0)
+		correct_size = 1
+		for item in content:
+
+			# If this is the correct number of threads
+			if item[0] == z:
+				# If this is the correct size 
+				if item[1] == correct_size:
+					# Add the time to the index of this size
+					ys.append(item[2])
+
+					# Go to the next size
+					correct_size += 1
+
+
+		# if flag == True:
+		# 	ys = [0, 3.1, 1.2, 1.6, 1.5, 2.3, 1.2, .7, .2, .9, .4]
+		# 	flag = False
+		# else:
+		# 	ys = [0, 3.1, .2, .6, .5, .3, 3.2, .7, .2, .9, .4]
+
+
+		# indexes 0 and -1 are equal to 0 because otherwise the graphs would float
+		ys[-1] = 0
+
+		# Add to list the values and the place in their graph (the size)
+		verts.append(list(zip(xs, ys)))
+
+
+	poly = PolyCollection(verts, facecolors=[cc('red'), cc('g'), cc('b'), cc('purple')])
+
+	#adiafaneia
+	#poly.set_alpha(0.7)
+	ax.add_collection3d(poly, zs=zs, zdir='y')
+
+	#Label axes and give the scale of them
+	ax.set_xlabel('Size')
+	ax.set_xlim3d(10, 15)
+	ax.set_ylabel('Proccessors')
+	ax.set_ylim3d(1, 5)
+	ax.set_zlabel('Time')
+	ax.set_zlim3d(0, 0.1)
+
+	# display the model
+	plt.show()
+
+
+main()
